8d3cef3abc63382ea11f876024275e04eeb5b68a
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2023-09-12 13:55:41 +0200

    Some styling

--- Task-02/main.py
+++ Task-02/main.py
@@ -35,14 +35,17 @@ style.map("TCombobox", fieldbackground=[("readonly", "#FFD700")])
 # Create a combo box (dropdown menu) for movie selection
 movie_titles = [title for title in movies_df['title']]
 combo = ttk.Combobox(
-    window, values=movie_titles, state="readonly", width=50)
+    window, values=movie_titles, state="readonly")
 combo.set("Select a movie")
 combo.bind("<<ComboboxSelected>>", on_movie_selection)
 combo.pack(pady=10)
 combo.focus()
 
+style.configure("TListbox", padding=5, background="#FFD700",
+                font=("Arial", 12), foreground="#000000")
+style.map("TListbox", background=[("active", "#FFD700")])
 # Create a listbox to display movie recommendations
-recommendation_listbox = tk.Listbox(window, width=300, height=20)
+recommendation_listbox = tk.Listbox(window, width=200, height=20)
 recommendation_listbox.pack()
 
 # Run the Tkinter main loop
